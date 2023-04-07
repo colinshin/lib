1474f650b4a33aa333a3656c369e6493de006725
李强 <570937581@qq.com>
2023-04-07 12:58:31 +0800

    1111111111111

--- utils/json/test/json_test.go
+++ utils/json/test/json_test.go
@@ -2,7 +2,10 @@ package json
 
 import (
 	json2 "CH123/Lib/utils/json"
+	"bytes"
 	"encoding/json"
+	"github.com/bytedance/sonic/decoder"
+	"strings"
 	"testing"
 )
 
@@ -10,6 +13,7 @@ import (
 func TestEncode(t *testing.T) {
 	data := map[string]string{"a": "<>asdf中国asdf%$", "b": "dddddddd<a href=\"\">asfdasdfasfd</a>"}
 	b, e := json2.EncodeEscape(&data)
+	_, _ = json2.Encode(&data)
 	b2, _ := json.Marshal(data)
 	var data1 map[string]string
 	//json2.Decode(b, &data1)
@@ -22,4 +26,23 @@ func TestEncode(t *testing.T) {
 	if string(b) != string(b2) {
 		t.Errorf("产生了错误的值 %s = %s", string(b), string(b2))
 	}
+}
+func TestStreamEncode(t *testing.T) {
+	var o1 = map[string]interface{}{
+		"a": "b",
+	}
+	var o2 = 1
+	var w = bytes.NewBuffer(nil)
+	var enc = json2.StreamEncode(w)
+	_ = enc.Encode(o1)
+	_ = enc.Encode(o2)
+	t.Log(w.String())
+}
+func TestStreamDecode(t *testing.T) {
+	var o = map[string]interface{}{}
+	var r = strings.NewReader(`{"a":"b"}{"1":"2"}`)
+	var dec = decoder.NewStreamDecoder(r)
+	dec.Decode(&o)
+	dec.Decode(&o)
+	t.Logf("%+v", o)
 }
