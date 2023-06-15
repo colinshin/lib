39aa34c428b874bfe5b4b72676090266b138a4be
flyerxp <570937581@qq.com>
2023-06-15 11:52:01 +0800

    1111111111111

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 
 require (
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/orcaman/concurrent-map/v2 v2.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
--- middleware/redisL/api.go
+++ middleware/redisL/api.go
@@ -8,16 +8,15 @@ import (
 	"github.com/flyerxp/lib/logger"
 	"github.com/flyerxp/lib/middleware/nacos"
 	yaml2 "github.com/flyerxp/lib/utils/yaml"
+	cmap "github.com/orcaman/concurrent-map/v2"
 	"github.com/redis/go-redis/v9"
 	"go.uber.org/zap"
-	"sync"
 	"time"
 )
 
 type redisClient struct {
-	RedisClient map[string]redis.UniversalClient
-	Lock        sync.Mutex
-	RedisConf   map[string]config.MidRedisConf
+	RedisClient cmap.ConcurrentMap[string, redis.UniversalClient]
+	RedisConf   cmap.ConcurrentMap[string, config.MidRedisConf]
 }
 
 var redisEngine *redisClient
@@ -26,15 +25,15 @@ func GetEngine(name string, ctx context.Context) (redis.UniversalClient, error) 
 	if redisEngine == nil {
 		redisEngine = new(redisClient)
 		var confList []config.MidRedisConf
-		redisEngine.RedisConf = make(map[string]config.MidRedisConf)
-		redisEngine.RedisClient = make(map[string]redis.UniversalClient)
+		redisEngine.RedisConf = cmap.New[config.MidRedisConf]()
+		redisEngine.RedisClient = cmap.New[redis.UniversalClient]()
 		conf := config2.GetConf()
 		confList = conf.Redis
 		//本地文件中获取
 		//redisEngine.Lock.Lock()
 		for _, v := range confList {
 			if v.Name != "" {
-				redisEngine.RedisConf[v.Name] = v
+				redisEngine.RedisConf.Set(v.Name, v)
 			}
 		}
 		//nacos获取
@@ -48,7 +47,7 @@ func GetEngine(name string, ctx context.Context) (redis.UniversalClient, error) 
 					e = yaml2.DecodeByBytes(yaml, redisList)
 					if e == nil {
 						for _, v := range redisList.Redis {
-							redisEngine.RedisConf[v.Name] = v
+							redisEngine.RedisConf.Set(v.Name, v)
 						}
 					} else {
 						logger.AddError(zap.Error(errors.New("yaml conver error")))
@@ -58,27 +57,23 @@ func GetEngine(name string, ctx context.Context) (redis.UniversalClient, error) 
 		}
 		//redisEngine.Lock.Unlock()
 	}
-	e, ok := redisEngine.RedisClient[name]
+	e, ok := redisEngine.RedisClient.Get(name)
 	if ok {
 		return e, nil
 	}
-	redisEngine.Lock.Lock()
-	if e, ok = redisEngine.RedisClient[name]; ok {
-		return e, nil
-	} else {
-		o, okC := redisEngine.RedisConf[name]
-		if okC {
-			redisEngine.RedisClient[name] = redis.NewUniversalClient(&redis.UniversalOptions{
-				Addrs:        o.Address,
-				MasterName:   o.Master,
-				Username:     o.User,
-				Password:     o.Pwd,
-				PoolTimeout:  time.Second,
-				MaxIdleConns: 30,
-			})
-			redisEngine.RedisClient[name].AddHook(HookLog{})
-			return redisEngine.RedisClient[name], nil
-		}
+	o, okC := redisEngine.RedisConf.Get(name)
+	if okC {
+		objRedis := redis.NewUniversalClient(&redis.UniversalOptions{
+			Addrs:        o.Address,
+			MasterName:   o.Master,
+			Username:     o.User,
+			Password:     o.Pwd,
+			PoolTimeout:  time.Second,
+			MaxIdleConns: 30,
+		})
+		objRedis.AddHook(HookLog{})
+		redisEngine.RedisClient.Set(name, objRedis)
+		return objRedis, nil
 	}
 	logger.AddError(zap.Error(errors.New("no find redis config " + name)))
 	return nil, errors.New("no find redis config " + name)
