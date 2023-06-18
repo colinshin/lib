98b6e726a94f1f92afab40faf79758cb7c3bc2c0
flyerxp <570937581@qq.com>
2023-06-18 18:10:51 +0800

    1111111111111

--- middleware/nacos/test/nacos_test.go
+++ middleware/nacos/test/nacos_test.go
@@ -3,7 +3,7 @@ package nacos
 import (
 	"context"
 	"fmt"
-	config2 "github.com/flyerxp/globalStruct/config"
+	"github.com/flyerxp/lib/config"
 	"github.com/flyerxp/lib/middleware/nacos"
 	yaml "github.com/flyerxp/lib/utils/yaml"
 	"testing"
@@ -12,7 +12,7 @@ import (
 func TestConf(t *testing.T) {
 	a, e := nacos.GetEngine("nacosConf", context.Background())
 	r, _ := a.GetConfig(context.Background(), "zk", "zookeeper", "62c3bcf9-7948-4c26-a353-cebc0a7c9712")
-	zk := new(config2.ZookeeperConf)
+	zk := new(config.ZookeeperConf)
 	_ = yaml.DecodeByBytes(r, zk)
 	fmt.Println(zk, e)
 }
